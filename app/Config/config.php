ab09f6eabaac9b8718fe1f6247e8e752cda324b1
raseloriginal <raseloriginal369@gmail.com>
2026-08-09 21:45:10 +0600

    feat: initialize application configuration file

--- app/Config/config.php
+++ app/Config/config.php
@@ -13,6 +13,7 @@ $isMainDomain = (
     $hostname === 'happybangladesh.com'  || $hostname === 'www.happybangladesh.com' ||
     $hostname === 'happybangladesh.site' || $hostname === 'www.happybangladesh.site'
 );
+$isv1 = ($hostname === 'v1.happybangladesh.com' || $hostname === 'www.v1.happybangladesh.com');
 $isDraft = ($hostname === 'happydraft.raseloriginal.digital');
 $isHttps = (!$isLocalhost)
     || (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off')
@@ -54,11 +55,17 @@ if ($isLocalhost) {
     define('DB_USER', 'happybd');
     define('DB_PASS', '9pH{53ff.uB5Qehh');
 } elseif ($isDraft) {
-  define('DB_HOST', 'localhost');
+    define('DB_HOST', 'localhost');
     define('DB_PORT', '3306');
     define('DB_NAME', 'happybddraft');
     define('DB_USER', 'happybddraft');
     define('DB_PASS', '9pH{53ff.uB5Qehh');
+} elseif ($isv1) {
+    define('DB_HOST', 'localhost');
+    define('DB_PORT', '3306');
+    define('DB_NAME', 'happybangladesh_v1');
+    define('DB_USER', 'happybangladesh_v1');
+    define('DB_PASS', 'OaSaTHTEbWrt5I607RTo');
 } else {
     define('DB_HOST', 'localhost');
     define('DB_PORT', '3306');
